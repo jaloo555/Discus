6ac9dc9c8e2374e20a3841d6b0d6acd09ae51f27
Jason <superj63@hotmail.com>
2016-11-23 20:25:01 +0800

    email

---
+++ config/initializers/rails_email_preview.rb
@@ -0,0 +1,33 @@
+require 'rails_email_preview'
+
+#= REP hooks and config
+RailsEmailPreview.setup do |config|
+
+ # hook before rendering preview:
+ config.before_render do |message, preview_class_name, mailer_action|
+   # apply premailer-rails:
+   Premailer::Rails::Hook.delivering_email(message)
+   # or actionmailer-inline-css:
+   ActionMailer::InlineCssHook.delivering_email(message)
+ end
+
+ # do not show Send Email button
+ config.enable_send_email = true
+
+ # You can specify a controller for RailsEmailPreview::ApplicationController to inherit from:
+ config.parent_controller = 'Admin::ApplicationController' # default: '::ApplicationController'
+end
+
+#= REP + Comfortable Mexican Sofa integration
+#
+# # enable comfortable_mexican_sofa integration:
+# require 'rails_email_preview/integrations/comfortable_mexica_sofa'
+
+Rails.application.config.to_prepare do
+  RailsEmailPreview.setup do |config|
+    config.layout            = 'application'
+    config.preview_classes   = Thredded::BaseMailerPreview.preview_classes
+    # Do not show Send Email button
+    config.enable_send_email = false
+  end
+end
